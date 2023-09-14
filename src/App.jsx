1b2fd6bf78a691edaf2ace45f9cf4dd6b3e39e64
TaanVeer <taanveer469@gmail.com>
2023-09-15 01:36:53 +0600

    alhamdulliah basic setup complete

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import Header from './components/header/header'
+import Blog from './components/blog/Blog'
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <Header></Header>
+      <Blog></Blog>
+   
+      
+     
+
+   
+    </>
+  )
+}
+
+export default App
---
+++ src/components/blog/Blog.jsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+
+const Blog = () => {
+    const [blog, setBlog] = useState([]);
+
+    useEffect(() => {
+        fetch('blogs.json')
+            .then(res => res.json())
+            .then(data => console.log(data))
+        
+    }, [])
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Blog;
