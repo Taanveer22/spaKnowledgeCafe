3dfd72cf963a9af0e050287f12ada321e91fc667
TaanVeer <taanveer469@gmail.com>
2023-09-15 19:04:13 +0600

    alhamdulliah blogs created and posted

--- src/components/blog/blog.jsx
+++ src/components/blog/blog.jsx
@@ -1,11 +1,44 @@
 import PropTypes from "prop-types";
 
 const Blog = ({ blog }) => {
-  console.log(blog);
-  return <div></div>;
+  const {
+    title,
+    cover,
+    author,
+    author_image,
+    posted_date,
+    reading_time,
+    hashtags,
+  } = blog;
+  return (
+    <div>
+      <img src={cover} />
+      <div className="flex justify-between items-center">
+        <div className="flex justify-around items-center gap-5">
+          <img src={author_image} className="w-14 text-xl" />
+          <div className="">
+            <h5>{author}</h5>
+            <p>{posted_date}</p>
+          </div>
+        </div>
+        <div className="">
+          <p>{reading_time} min read</p>
+          <button>new</button>
+        </div>
+      </div>
+      <h2 className="text-3xl">{title}</h2>
+      <p>
+        {hashtags.map((hash, index) => (
+          <span key={index}>
+            <a href="#"> #{hash}</a>
+          </span>
+        ))}
+      </p>
+    </div>
+  );
 };
 
 Blog.propTypes = {
-    blog: PropTypes.object.isrequired
-}
+  blog: PropTypes.object,
+};
 export default Blog;
--- src/components/blogs/Blogs.jsx
+++ src/components/blogs/Blogs.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-
+import Blog from "../blog/Blog";
 
 const Blogs = () => {
     const [blogs, setBlogs] = useState([]);
@@ -12,7 +12,10 @@ const Blogs = () => {
     }, [])
     return (
         <div className="md:w-2/3">
-            <h2 className="text-3xl">blogs: { blogs.length }</h2>
+            <h2 className="text-3xl">blogs: {blogs.length}</h2>
+            {
+              blogs.map(blog => <Blog key={blog.id} blog={blog}></Blog>)
+            }
             
         </div>
     );
